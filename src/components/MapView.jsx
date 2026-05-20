a3bf768a2082f1fc35381b9337811673d0bb3f08
Jair Gabriel <jairdj14@gmail.com>
2026-05-20 12:24:55 -0600

    openstreetmap change

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -473,8 +473,8 @@ export default function MapView({
         <FollowUser users={users} followUserId={followUserId} />
 
         <TileLayer
-          attribution="&copy; Stadia Maps"
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+          attribution="&copy; OpenStreetMap contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
         {/* USERS */}
